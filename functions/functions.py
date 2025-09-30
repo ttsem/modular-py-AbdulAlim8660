a248d9865613e36f29daea548e2ac8db38809fab
Abdul Alim Baig <71715745+AbdulAlim8660@users.noreply.github.com>
2025-09-30 19:42:53 +0530

    updated the colours list in the functions.py

--- functions/functions.py
+++ functions/functions.py
@@ -1,3 +1,6 @@
+MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
+MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
+
 def color_pair_to_string(major_color, minor_color):
   return f'{major_color} {minor_color}'
 
